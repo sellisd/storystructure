73008f6029bb4347e4b3d532b18fa7e32b47e419
Diamantis Sellis <sellisd@gmail.com>
2018-10-07 12:03:24 +0200

    make visualization before and after simplification

--- src/test.py
+++ src/test.py
@@ -4,9 +4,18 @@ from subprocess import call
 from storystructure import storystructure
 a = storystructure()
 a.loadStory(edges = "../data/dolmadakia/edgelist.csv", nodes = "../data/dolmadakia/nodeAttributes.csv")
-a.graph.setRoot(a.graph.nodes[7])
-print([i.id for i in a.getStart()])
 a.saveDot("../data/dolmadakia/structure.dot")
-# dot ../data/88_ntolmadakia.dot -Tpng > ../figs/88_ntolmadakia.png
 f = open("../figs/dolmadakia.png", "w")
 call(["dot", "../data/dolmadakia/structure.dot", "-Tpng"], stdout = f)
+
+a.simplify()
+#a.makeGraph()
+a.saveDot("../data/dolmadakia/structureSimplified.dot")
+f = open("../figs/dolmadakiaSimplified.png", "w")
+call(["dot", "../data/dolmadakia/structureSimplified.dot", "-Tpng"], stdout = f)
+
+a.makeGraph()
+print([i.id for i in a.getStart()])
+a.graph.setRoot(a.graph.nodes[7])
+
+a.savePathStats(7, '../data/dolmadakia/allPaths.dat')
